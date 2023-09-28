4297b30dcea36e0490060e0b8580d8855844ee89
erikbozic <bozic.erik@gmail.com>
2023-09-28 16:35:13 +0200

    chore: create a new migration on windows and get some changes even though model unchanged

---
+++ Migrations/20230928142257_Migration2Windows.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreRepro.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration2Windows : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_TestEntity_TestEnum2",
+                table: "TestEntity");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_TestEntity_TestEnum2",
+                table: "TestEntity",
+                sql: "\r\n  (Status = 'Status1' AND SubStatus IN ('Status1_SubStatus1','Status1_SubStatus2','Status1_SubStatus3','Status1_SubStatus4','Status1_SubStatus5','Status1_SubStatus6') OR\r\n Status = 'Status2' AND SubStatus IN ('Status2_SubStatus1','Status2_SubStatus2','Status2_SubStatus3','Status2_SubStatus4')))\r\n");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_TestEntity_TestEnum2",
+                table: "TestEntity");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_TestEntity_TestEnum2",
+                table: "TestEntity",
+                sql: "\n  (Status = 'Status1' AND SubStatus IN ('Status1_SubStatus1','Status1_SubStatus2','Status1_SubStatus3','Status1_SubStatus4','Status1_SubStatus5','Status1_SubStatus6') OR\n Status = 'Status2' AND SubStatus IN ('Status2_SubStatus1','Status2_SubStatus2','Status2_SubStatus3','Status2_SubStatus4')))\n");
+        }
+    }
+}
